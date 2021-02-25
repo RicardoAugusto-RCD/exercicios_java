b1f773981d8985d3a6f53dfc9a70cbb746980f91
Ricardo Augusto <ricardoaugusto.rcd@gmail.com>
2021-02-25 15:46:09 -0300

    Primeiro Programa

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+
+package primeiroprograma;
+
+
+public class PrimeiroPrograma {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo");
+    }
+    
+}
+    
+
+    
+
